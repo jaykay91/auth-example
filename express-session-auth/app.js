05a3a4d557a1f4ac0b399f2a5e6897db8567818f
JayKay <jaykay91.me@gmail.com>
2020-02-19 17:49:58 +0900

    session 저장소 redis로 변경

--- express-session-auth/app.js
+++ express-session-auth/app.js
@@ -6,38 +6,52 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
 const flash = require("connect-flash");
 
 dotenv.config();
 
-const connect = require("./schemas");
-const passportConfig = require("./passport");
+const connectMongo = require("./schemas");
+const configPassport = require("./passport");
+
 const indexRouter = require("./routes");
 const authRouter = require("./routes/auth");
 
 const app = express();
-connect();
-passportConfig(passport);
+connectMongo();
+configPassport(passport);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+} else {
+  app.use(morgan("dev"));
+}
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false
-    }
+
+const sessionOpts = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false
+  },
+  store: new RedisStore({
+    host: "localhost",
+    port: 1234,
+    logErrors: true
   })
-);
+};
+
+app.use(session(sessionOpts));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -55,7 +69,6 @@ app.use((err, req, res, next) => {
   res.json(err);
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is created at http://localhost:${PORT}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server is created at http://localhost:${app.get("port")}`);
 });
